241782fc8b0c05da30e2c34222619ae0e6b004de
Ahmad Shahir Abdul Satar <abdulsataas2@vcu.edu>
2017-05-03 00:06:15 -0400

    basic setup

--- QuizApp/MultiplayerManager.swift
+++ QuizApp/MultiplayerManager.swift
@@ -9,66 +9,71 @@
 import UIKit
 import MultipeerConnectivity
 
-class MPCManager: UIViewController {
+class MPCManager: UIViewController, MCBrowserViewControllerDelegate, MCSessionDelegate {
     
     var session: MCSession!
-    
+
     var peerID: MCPeerID!
     
-    var browser: MCNearbyServiceBrowser!
+    var browser: MCBrowserViewController!
     
-    var advertiser: MCNearbyServiceAdvertiser!
-    
-    
-    var foundPeers = [MCPeerID]()
-    
-    var invitationHandler: ((Bool, MCSession?)->Void)!
-    
+    var advertiser: MCAdvertiserAssistant? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        peerID = MCPeerID(displayName: UIDevice.current.name)
-        session = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .required)
-        //session.delegate = self
         
+        self.peerID = MCPeerID(displayName: UIDevice.current.name)
+        self.session = MCSession(peer: peerID)
+        self.browser = MCBrowserViewController(serviceType: "quiz", session: session)
+        self.advertiser = MCAdvertiserAssistant(serviceType: "quiz", discoveryInfo: nil, session: session)
         
-        
-        
-        
-        
-        
-        
+        advertiser?.start()
+        session.delegate = self
+        browser.delegate = self
+    }
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+            switch state {
+            case MCSessionState.connected:
+                print("Connected: \(peerID.displayName)")
+            case MCSessionState.connecting:
+                print("Connecting: \(peerID.displayName)")
+            case MCSessionState.notConnected:
+                print("Not Connected: \(peerID.displayName)")
+            }
+            
         
     }
     
-    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        
+    
+    }
+        
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL, withError error: Error?) {
         
     }
     func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
         
     }
-    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL, withError error: Error?) {
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
         
     }
-    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
-        dismiss(animated: true)
-    }
     
-    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
-        dismiss(animated: true)
-    }
     
-    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
-        switch state {
-        case MCSessionState.connected:
-            print("Connected: \(peerID.displayName)")
-        case MCSessionState.connecting:
-            print("Connecting: \(peerID.displayName)")
-        case MCSessionState.notConnected:
-            print("Not Connected: \(peerID.displayName)")
-        }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
+  
     
     
     
